2573cee2623e275af91313e549ed8e7ecffe4ee6
Banyc <36535895+Banyc@users.noreply.github.com>
2022-07-02 22:59:00 +0800

    feat: backpropagate

--- src/nodes/node.rs
+++ src/nodes/node.rs
@@ -3,7 +3,12 @@ use std::sync::Arc;
 use super::utils::cached_node_data::CachedNodeData;
 
 pub trait NodeComputation {
-    fn compute_output(&self, parameters: &Vec<f64>, operand_outputs: &Vec<f64>) -> f64;
+    fn compute_output(
+        &self,
+        parameters: &Vec<f64>,
+        operand_outputs: &Vec<f64>,
+        inputs: &Vec<f64>,
+    ) -> f64;
     fn compute_local_operand_gradient(
         &self,
         parameters: &Vec<f64>,
@@ -63,22 +68,6 @@ impl GeneralNode {
         this
     }
 
-    pub fn do_gradient_descent_step(&mut self, step_size: f64) {
-        let gradient = self.global_parameter_gradient();
-        for (i, gradient_entry) in gradient.iter().enumerate() {
-            self.parameters[i] -= step_size * *gradient_entry;
-        }
-        self.check_rep();
-    }
-
-    pub fn distribute_global_gradient_entries_to_operands(&mut self) {
-        for i in 0..self.operands.len() {
-            let gradient_entry = self.global_gradient() * self.local_operand_gradient()[i];
-            self.operands[i].add_global_gradient_entry(gradient_entry);
-        }
-        self.check_rep();
-    }
-
     pub fn evaluate(&mut self, inputs: &Vec<f64>) -> f64 {
         self.cache.output.get_or_insert_with(|| {
             assert!(self.cache.operand_outputs.is_none());
@@ -88,7 +77,7 @@ impl GeneralNode {
             }
             let ret = self
                 .computation
-                .compute_output(&self.parameters, &operand_outputs);
+                .compute_output(&self.parameters, &operand_outputs, inputs);
             self.cache.operand_outputs = Some(Arc::new(operand_outputs));
             ret
         });
@@ -97,11 +86,38 @@ impl GeneralNode {
         self.cache.output.unwrap()
     }
 
+    pub fn backpropagate(&mut self, step_size: f64) {
+        self.distribute_global_gradient_entries_to_operands();
+        self.do_gradient_descent_step(step_size);
+        self.cache.reset();
+    }
+
+    fn do_gradient_descent_step(&mut self, step_size: f64) {
+        let gradient = self.global_parameter_gradient();
+        for (i, gradient_entry) in gradient.iter().enumerate() {
+            self.parameters[i] -= step_size * *gradient_entry;
+        }
+        self.check_rep();
+    }
+
+    fn distribute_global_gradient_entries_to_operands(&mut self) {
+        for i in 0..self.operands.len() {
+            let gradient_entry = self.global_gradient() * self.local_operand_gradient()[i];
+            self.operands[i].add_global_gradient_entry(gradient_entry);
+        }
+        self.check_rep();
+    }
+
     fn add_global_gradient_entry(&mut self, gradient_entry: f64) {
         assert!(self.cache.global_gradient.is_none());
         self.cache.global_gradient_entries.push(gradient_entry);
     }
 
+    /// $$
+    /// \frac{\partial f}{\partial z}
+    /// $$
+    ///
+    /// - $z$: the non-tunable operands of $f$
     fn local_operand_gradient(&mut self) -> Arc<Vec<f64>> {
         // TODO: throw error if None
         let operand_outputs = self.operand_outputs().unwrap();
@@ -115,6 +131,11 @@ impl GeneralNode {
         Arc::clone(self.cache.local_operand_gradient.as_ref().unwrap())
     }
 
+    /// $$
+    /// \frac{\partial E}{\partial f}
+    /// $$
+    ///
+    /// - $E$: the out-most function of the entire network
     fn global_gradient(&mut self) -> f64 {
         self.cache.global_gradient.get_or_insert_with(|| {
             // TODO: assert successor_len = global_gradient_entries.len()
@@ -124,6 +145,11 @@ impl GeneralNode {
         self.cache.global_gradient.unwrap()
     }
 
+    /// $$
+    /// \frac{\partial f}{\partial w}
+    /// $$
+    ///
+    /// - $w$: the tunable parameters of $f$
     fn local_parameter_gradient(&mut self) -> Arc<Vec<f64>> {
         // TODO: throw error if None
         let operand_outputs = self.operand_outputs().unwrap();
@@ -137,6 +163,11 @@ impl GeneralNode {
         Arc::clone(self.cache.local_parameter_gradient.as_ref().unwrap())
     }
 
+    /// $$
+    /// \frac{\partial E}{\partial w}
+    /// $$
+    ///
+    /// - $w$: the tunable parameters of $f$
     fn global_parameter_gradient(&mut self) -> Arc<Vec<f64>> {
         let local_parameter_gradient = self.local_parameter_gradient();
         let global_gradient = self.global_gradient();

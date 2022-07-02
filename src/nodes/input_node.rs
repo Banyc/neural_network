a3613e9ab05ae94d71d82a46cd491805e25da1b4
Banyc <36535895+Banyc@users.noreply.github.com>
2022-07-02 23:44:48 +0800

    feat: input node

---
+++ src/nodes/input_node.rs
@@ -0,0 +1,38 @@
+use super::node::{GeneralNode, NodeComputation};
+
+pub fn input_node(input_index: usize) -> GeneralNode {
+    let computation = InputNodeComputation { input_index };
+    let node = GeneralNode::new(Vec::new(), Box::new(computation), Vec::new());
+    node
+}
+
+pub struct InputNodeComputation {
+    input_index: usize,
+}
+
+impl NodeComputation for InputNodeComputation {
+    fn compute_output(
+        &self,
+        _parameters: &Vec<f64>,
+        _operand_outputs: &Vec<f64>,
+        inputs: &Vec<f64>,
+    ) -> f64 {
+        inputs[self.input_index]
+    }
+
+    fn compute_local_operand_gradient(
+        &self,
+        _parameters: &Vec<f64>,
+        _operand_outputs: &Vec<f64>,
+    ) -> Vec<f64> {
+        Vec::new()
+    }
+
+    fn compute_local_parameter_gradient(
+        &self,
+        _parameters: &Vec<f64>,
+        _operand_outputs: &Vec<f64>,
+    ) -> Vec<f64> {
+        Vec::new()
+    }
+}
--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -1,2 +1,3 @@
+pub mod input_node;
 pub mod node;
 pub mod utils;

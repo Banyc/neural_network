17d4504f603531db04a0b9f6f4ebaf0c8eaf3c96
Banyc <36535895+Banyc@users.noreply.github.com>
2022-07-03 00:12:38 +0800

    feat: ReLU node

--- src/nodes/mod.rs
+++ src/nodes/mod.rs
@@ -1,4 +1,5 @@
 pub mod input_node;
 pub mod node;
+pub mod relu_node;
 pub mod sigmoid_node;
 pub mod utils;
---
+++ src/nodes/relu_node.rs
@@ -0,0 +1,49 @@
+use super::node::{GeneralNode, NodeComputation};
+
+pub fn relu_node(operand: GeneralNode) -> GeneralNode {
+    let computation = ReluNodeComputation {};
+    let node = GeneralNode::new(vec![operand], Box::new(computation), Vec::new());
+    node
+}
+
+struct ReluNodeComputation {}
+
+impl NodeComputation for ReluNodeComputation {
+    fn compute_output(
+        &self,
+        _parameters: &Vec<f64>,
+        operand_outputs: &Vec<f64>,
+        _inputs: &Vec<f64>,
+    ) -> f64 {
+        assert_eq!(operand_outputs.len(), 1);
+        relu(operand_outputs[0])
+    }
+
+    fn compute_local_operand_gradient(
+        &self,
+        _parameters: &Vec<f64>,
+        operand_outputs: &Vec<f64>,
+    ) -> Vec<f64> {
+        assert_eq!(operand_outputs.len(), 1);
+        vec![relu_derivative(operand_outputs[0])]
+    }
+
+    fn compute_local_parameter_gradient(
+        &self,
+        _parameters: &Vec<f64>,
+        _operand_outputs: &Vec<f64>,
+    ) -> Vec<f64> {
+        Vec::new()
+    }
+}
+
+fn relu(x: f64) -> f64 {
+    f64::max(x, 0.0)
+}
+
+fn relu_derivative(x: f64) -> f64 {
+    match x {
+        _ if x > 0.0 => 1.0,
+        _ => 0.0,
+    }
+}

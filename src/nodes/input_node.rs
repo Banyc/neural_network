523e29b271120937a6a16c563bdb48ac3ae7e788
Banyc <36535895+Banyc@users.noreply.github.com>
2022-07-03 19:55:48 +0800

    feat: input node batch

--- src/nodes/input_node.rs
+++ src/nodes/input_node.rs
@@ -1,9 +1,20 @@
+use std::sync::{Arc, Mutex};
+
 use super::node::{GeneralNode, NodeComputation};
 
 pub fn input_node(input_index: usize) -> GeneralNode {
     let computation = InputNodeComputation { input_index };
     let node = GeneralNode::new(Vec::new(), Box::new(computation), Vec::new());
     node
+}
+
+pub fn input_node_batch(len: usize) -> Vec<Arc<Mutex<GeneralNode>>> {
+    let mut nodes = Vec::new();
+    for i in 0..len {
+        let node = input_node(i);
+        nodes.push(Arc::new(Mutex::new(node)));
+    }
+    nodes
 }
 
 struct InputNodeComputation {
@@ -45,26 +56,22 @@ mod tests {
     fn node_output1() {
         let mut node = input_node(0);
         let ret = node.evaluate(&vec![3.0]);
-        assert!(ret >= 3.0);
-        assert!(ret <= 3.0);
+        assert_eq!(ret, 3.0);
     }
 
     #[test]
     fn node_output2() {
         let mut node = input_node(0);
         let ret = node.evaluate(&vec![-4.0]);
-        assert!(ret >= -4.0);
-        assert!(ret <= -4.0);
+        assert_eq!(ret, -4.0);
     }
 
     #[test]
     fn computation_output() {
         let computation = InputNodeComputation { input_index: 0 };
         let ret = computation.compute_output(&vec![], &vec![], &vec![3.0]);
-        assert!(ret >= 3.0);
-        assert!(ret <= 3.0);
+        assert_eq!(ret, 3.0);
         let ret = computation.compute_output(&vec![], &vec![], &vec![-4.0]);
-        assert!(ret >= -4.0);
-        assert!(ret <= -4.0);
+        assert_eq!(ret, -4.0);
     }
 }

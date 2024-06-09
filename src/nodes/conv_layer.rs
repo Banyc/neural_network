927274b5c92afc32f657593f809b9ffbe2c34f70
Banyc <36535895+Banyc@users.noreply.github.com>
2024-06-09 16:43:43 +0800

    feat: deep conv layer

--- src/nodes/conv_layer.rs
+++ src/nodes/conv_layer.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    num::NonZeroUsize,
+    sync::{Arc, Mutex},
+};
 
 use crate::{
     node::Node,
@@ -11,10 +14,11 @@ use super::{
     linear_node::{self, linear_node},
 };
 
+#[derive(Clone)]
 pub struct KernelConfig<'a> {
     pub shape: &'a NonZeroShape,
-    pub initial_weights: Option<Box<dyn Fn() -> Vec<f64>>>,
-    pub initial_bias: Option<Box<dyn Fn() -> f64>>,
+    pub initial_weights: Option<&'a dyn Fn() -> Vec<f64>>,
+    pub initial_bias: Option<&'a dyn Fn() -> f64>,
     pub lambda: Option<f64>,
 }
 
@@ -34,8 +38,9 @@ pub fn conv_layer(
         let weights = kernel.initial_weights.as_ref().map(|f| f());
         let bias = kernel.initial_bias.as_ref().map(|f| f());
         let param_injection = param_injection.as_mut().map(|x| {
-            let weights_name = format!("{}:kernel.{}:weights", x.name, params.i);
-            let bias_name = format!("{}:kernel.{}:bias", x.name, params.i);
+            let prefix = format!("{}:kernel.{}", x.name, params.i);
+            let weights_name = format!("{prefix}:weights");
+            let bias_name = format!("{prefix}:bias");
             linear_node::ParamInjection {
                 injector: x.injector,
                 weights_name,
@@ -47,4 +52,35 @@ pub fn conv_layer(
         feature_node.unwrap()
     };
     kernel_layer(inputs, stride, kernel.shape, create_filter)
+}
+
+pub fn deep_conv_layer(
+    inputs: Tensor<'_, Arc<Mutex<Node>>>,
+    stride: &Stride,
+    kernel: KernelConfig,
+    depth: NonZeroUsize,
+    mut param_injection: Option<ParamInjection<'_>>,
+) -> (Vec<Vec<Arc<Mutex<Node>>>>, OwnedShape) {
+    let mut kernel_layers = vec![];
+    let mut kernel_layer_shape = None;
+    for depth in 0..depth.get() {
+        let param_injection = match &mut param_injection {
+            Some(x) => {
+                let name = &x.name;
+                let name = format!("{name}:depth.{depth}");
+                Some(ParamInjection {
+                    injector: x.injector,
+                    name,
+                })
+            }
+            None => None,
+        };
+        let (layer, shape) = conv_layer(inputs, stride, kernel.clone(), param_injection);
+        if let Some(layer_shape) = &kernel_layer_shape {
+            assert_eq!(&shape, layer_shape);
+        }
+        kernel_layer_shape = Some(shape);
+        kernel_layers.push(layer);
+    }
+    (kernel_layers, kernel_layer_shape.unwrap())
 }
